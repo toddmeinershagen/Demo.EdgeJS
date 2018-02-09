950d288cf1d468dbb16be36fba365598718380a2
Meinershagen <tmeinershagen@nthrive.com>
2018-02-09 13:10:18 -0600

    Initial commit.

---
+++ src/Demo.ClrToNode/Demo.ClrToNode/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using EdgeJs;
+using Newtonsoft.Json;
+
+namespace Demo.ClrToNode
+{
+    class Program
+    {
+        public static async Task Start()
+        {
+            //var func = Edge.Func(@"return require('./../script.js')");
+
+            var script = File.ReadAllText("script.js");
+            var func = Edge.Func(script);
+
+            //var person = new Person {FirstName = "Todd", LastName = "Meinershagen"};
+            var person = new Person { FirstName = "Tammy", LastName = "Meinershagen" };
+
+            var message = await func(person);
+            await Console.Out.WriteLineAsync(message.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Start().Wait();
+            }
+            catch (Exception ex)
+            {
+                //var realEx = JsonConvert.DeserializeObject<CustomError>(ex.InnerException.Message) ?? ex.InnerException;
+                var realEx = GetException(ex);
+                Console.WriteLine(realEx);
+            }
+            
+            Console.ReadLine();
+        }
+
+        private static Exception GetException(Exception ex)
+        {
+            var errorType = ex.InnerException.Message.Split(':').FirstOrDefault();
+            if (errorType == "BadRequestError")
+            {
+                return new BadRequestException(ex.InnerException.Message);
+            }
+
+            return ex;
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        { }
+    }
+}
---
+++ src/Demo.ClrToNode/Demo.ClrToNode/script.js
@@ -0,0 +1,66 @@
+﻿/*function CustomError(foo, message, fileName, lineNumber) {
+  var instance = new Error(message, fileName, lineNumber);
+  instance.foo = foo;
+  Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+  if (Error.captureStackTrace) {
+    Error.captureStackTrace(instance, CustomError);
+  }
+  return instance;
+}
+
+CustomError.prototype = Object.create(Error.prototype, {
+  constructor: {
+    value: Error,
+    enumerable: false,
+    writable: true,
+    configurable: true
+  }
+});
+
+if (Object.setPrototypeOf){
+  Object.setPrototypeOf(CustomError, Error);
+} else {
+  CustomError.__proto__ = Error;
+}*/
+
+/*
+function CustomError() {
+    this.Type = "";
+    this.IsBadRequest = false;
+    this.Message = "";
+}
+*/
+
+//module.exports = function (data, callback) {
+var display = function (data, callback) {
+    //throw new Error("something bad happened");
+    //var error = new CustomError();
+    //error.Type = "BadRequestError";
+    //error.IsBadRequest = true;
+    //error.Message = "Something bad happened and it was related to a bad client request.";
+
+    //callback(new Error(JSON.stringify(error)));
+
+    if (data.FirstName === "Todd") {
+        var error = new Error("'Todd' is not a suitable value for the First Name.");
+        error.name = "BadRequestError";
+        callback(error);
+    }
+
+    //var error2 = new Error("something bad happened");
+    //Object.defineProperty(error2, "IsBadRequest");
+    //Object.defineProperty(error2, "Type");
+    //error2.IsBadRequest = true;
+    //error2.Type = "BadRequestError";
+
+    //error2["IsBadRequest"] = true;
+    //callback(error2);
+
+    //callback(new Error("something bad happened"));
+    //callback(new CustomError('bar', 'What??'));
+
+    callback(null, "Welcome to NodeJS, " + data.FirstName + " " + data.LastName + ".");
+};
+
+//display("test", console.log);
+return display;
